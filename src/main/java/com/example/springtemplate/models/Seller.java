bfbb78787d1c77d1e09a6b7e2d5835bab204ee45
Patrick Etka <patricketka@gmail.com>
2021-12-02 22:25:03 -0500

    Added domain classes

--- src/main/java/com/example/springtemplate/models/Seller.java
+++ src/main/java/com/example/springtemplate/models/Seller.java
@@ -1,17 +1,26 @@
 package com.example.springtemplate.models;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name = "sellers")
-@PrimaryKeyJoinColumn(name = "userId")
+@PrimaryKeyJoinColumn(name = "user_id")
 public class Seller extends User{
     public String companyName;
     public Integer yearFounded;
+    @ManyToMany
+    @JoinTable(name = "follows",
+    joinColumns = @JoinColumn(name="seller_id",
+    referencedColumnName = "userId"),
+    inverseJoinColumns = @JoinColumn(name = "customer_id",
+    referencedColumnName = "userId"))
+    public List<Customer> customerList;
 
-    public Seller(String companyName, Integer yearFounded) {
+    public Seller(String companyName, Integer yearFounded, List<Customer> customerList) {
         this.companyName = companyName;
         this.yearFounded = yearFounded;
+        this.customerList = customerList;
     }
 
     public Seller() {}
@@ -30,6 +39,14 @@ public class Seller extends User{
 
     public void setYearFounded(Integer yearFounded) {
         this.yearFounded = yearFounded;
+    }
+
+    public List<Customer> getCustomerList() {
+        return this.customerList;
+    }
+
+    public void addCustomer(Customer customer) {
+        this.customerList.add(customer);
     }
 
 }
